5da73aeb742a79d6d5936843dfa2d7b2daf9e8b5
Beatrize <heybeatrize@gmail.com>
2019-07-25 11:51:36 -0700

    set up, not working as it should

--- guessing_game.py
+++ guessing_game.py
@@ -1,6 +1,6 @@
 import random
 
-random_number = random.randint(1, 10) # numbers 1 - 10
+random_number = random.randint(1, 2) # numbers 1 - 10
 
 #handle user guesses
 # if they guess correct, tell them they won
@@ -8,3 +8,9 @@ random_number = random.randint(1, 10) # numbers 1 - 10
 
 #Bonus - let player play again if they want
 
+message = int(input("Guess a number between 1 to 10: "))
+
+
+while message != random_number:
+	message = int(input(f"{message} is wrong. Try again: "))
+print(f"{message} is correct!")
