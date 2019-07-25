294bcdd91d2e8a4a4207aa8693b2da7586d74481
Beatrize <heybeatrize@gmail.com>
2019-07-25 11:58:50 -0700

    omg works

--- guessing_game.py
+++ guessing_game.py
@@ -1,6 +1,6 @@
 import random
 
-random_number = random.randint(1, 2) # numbers 1 - 10
+random_number = random.randint(1, 10) # numbers 1 - 10
 
 #handle user guesses
 # if they guess correct, tell them they won
@@ -12,5 +12,8 @@ message = int(input("Guess a number between 1 to 10: "))
 
 
 while message != random_number:
-	message = int(input(f"{message} is wrong. Try again: "))
+	if message < random_number:
+		message = int(input(f"{message} is too low. Try again: "))
+	if message > random_number:
+		message = int(input(f"{message} is too high. Try again: "))
 print(f"{message} is correct!")
